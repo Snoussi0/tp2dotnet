b0ea76412febd7238192e7bb2a7d84ddb291ac75
Snoussi0 <48160386+Snoussi0@users.noreply.github.com>
2020-01-10 21:15:56 +0100

    firstcommit

---
+++ TP2_dotnet/TP2/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace TP2
+{
+    
+    
+        public partial class Form1 : Form
+        {
+            private SqlConnection cn = new SqlConnection("Data Source=ASUS/PC-BAHA; initial catalog=Clients; integrated security=true");
+
+
+            private bool rechercher_cin(string s) //fonction qui permet de vérifier l'existence d'un client
+            {
+                bool p = false;
+                cn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = "select * from client where cin='" + s + "'";
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                    p = true;
+
+                dr.Close();
+                cn.Close();
+                return p;
+            }
+
+            private void actualiser()   //procédure qui permet de charger le contenu de la table client dans le Datagridview
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("select * from client", cn);
+                SqlDataReader dr = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = dt;
+                dr.Close();
+                cn.Close();
+            }
+
+
+
+
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+
+                actualiser(); //charger le contenu de la table client dans le datagridwiew au démarrage du formulaire
+            }
+
+            private void btn_nouveau_Click(object sender, EventArgs e) //vider les champs du formulaire
+            {
+                txt_cin.Text = "";
+                txt_nom.Text = "";
+                txt_prenom.Text = "";
+                txt_ville.Text = "";
+                txt_tel.Text = "";
+            }
+
+            private void btn_rechercher_Click(object sender, EventArgs e)
+            {
+                if (txt_cin.Text == "") //controle de saisie sur le champ CIN
+                    MessageBox.Show("Champ CIN vide!!", "champ vide", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                else if (rechercher_cin(txt_cin.Text) == false) // vérification de l'existence du client dont le CIN est saisi dans la zone CIN
+                {
+                    txt_nom.Text = ""; // vider les valeurs des recherches précédentes
+                    txt_prenom.Text = "";
+                    txt_ville.Text = "";
+                    txt_tel.Text = "";
+                    MessageBox.Show("Client introuvable", "introuvable", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    txt_cin.Focus();
+                    txt_cin.SelectAll(); //selectionner le champ CIN pour une éventuelle resaisie
+                }
+                else
+                {
+                    cn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = cn;
+                    cmd.CommandText = "select * from client where cin='" + txt_cin.Text + "'";
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    dr.Read(); // un seul client 
+                    txt_nom.Text = dr[1].ToString();
+                    txt_prenom.Text = dr[2].ToString();
+                    txt_ville.Text = dr[3].ToString();
+                    txt_tel.Text = dr[4].ToString();
+                    dr.Close();
+                    cn.Close();
+
+                }
+            }
+
+            private void btn_ajouter_Click(object sender, EventArgs e)
+            {
+                if (txt_cin.Text == "" || txt_nom.Text == "" || txt_prenom.Text == "" || txt_ville.Text == "" || txt_tel.Text == "") //controle de saisie
+                {
+                    MessageBox.Show("Champ vide!!", "champ vide", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else if (rechercher_cin(txt_cin.Text) == true) // vérification de l'unicité du client dont le CIN est saisi dans la zone CIN
+                {
+
+                    MessageBox.Show("Client existe déja", "existant", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    txt_cin.Focus();
+                    txt_cin.SelectAll();
+                }
+                else
+                {
+                    cn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = cn;
+                    cmd.CommandText = String.Format("insert into client values('{0}','{1}','{2}','{3}','{4}')", txt_cin.Text, txt_nom.Text, txt_prenom.Text, txt_ville.Text, txt_tel.Text);
+                    int r = cmd.ExecuteNonQuery(); // ou ecrire cmd.ExecuteNonQuery() sans retour
+                    if (r != 0)
+                    {
+                        MessageBox.Show("Client bien ajouté", "ajout", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        cn.Close(); // fermer la connexion avant d'appeler la méthode actualiser
+                        actualiser(); //charger à nouveau les données pour afficher le nouveau client
+                    }
+
+                }
+
+            }
+
+            private void btn_modifier_Click(object sender, EventArgs e)
+            {
+                if (txt_cin.Text == "" || txt_nom.Text == "" || txt_prenom.Text == "" || txt_ville.Text == "" || txt_tel.Text == "") //controle de saisie
+                {
+                    MessageBox.Show("Champ vide!!", "champ vide", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else if (rechercher_cin(txt_cin.Text) == false) // vérification de l'existence du client dont le CIN est saisi dans la zone CIN
+                {
+
+                    MessageBox.Show("Client Introuvable", "Introuvable", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    txt_cin.Focus();
+                    txt_cin.SelectAll(); //selectionner le champ CIN pour une éventuelle resaisie
+                }
+                else
+                {
+                    cn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = cn;
+                    cmd.CommandText = String.Format("update client set nom='{0}',prenom='{1}',ville='{2}',tel='{3}' where cin='{4}'", txt_nom.Text, txt_prenom.Text, txt_ville.Text, txt_tel.Text, txt_cin.Text);
+                    int r = cmd.ExecuteNonQuery();
+                    if (r != 0)
+                    {
+                        MessageBox.Show("Client bien Modifié", "Modification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        cn.Close();
+                        actualiser();
+                    }
+
+                }
+            }
+
+            private void btn_supprimer_Click(object sender, EventArgs e)
+            {
+                if (txt_cin.Text == "") //controle de saisie
+                {
+                    MessageBox.Show("Champ CIN vide!!", "champ vide", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else if (rechercher_cin(txt_cin.Text) == false) // vérification de l'existence du client dont le CIN est saisi dans la zone CIN
+                {
+
+                    MessageBox.Show("Client Introuvable", "Introuvable", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    txt_cin.Focus();
+                    txt_cin.SelectAll(); //selectionner le champ CIN pour une éventuelle resaisie
+                }
+                else
+                {
+                    cn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = cn;
+                    cmd.CommandText = String.Format("delete from client where cin='{0}'", txt_cin.Text);
+                    int r = cmd.ExecuteNonQuery();
+                    if (r != 0)
+                    {
+                        MessageBox.Show("Client bien Supprimé", "Suppression", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        btn_nouveau.PerformClick(); //appeler le bouton nouveau pour vider les champs
+                        cn.Close();
+                        actualiser();
+                    }
+
+                }
+            }
+
+            private void btn_quitter_Click(object sender, EventArgs e)
+            {
+                this.Close();
+            }
+        }
+    }
+ 
---
+++ TP2_dotnet/TP2/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_cin = new System.Windows.Forms.TextBox();
+            this.txt_nom = new System.Windows.Forms.TextBox();
+            this.txt_prenom = new System.Windows.Forms.TextBox();
+            this.txt_ville = new System.Windows.Forms.TextBox();
+            this.txt_tel = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_nouveau = new System.Windows.Forms.Button();
+            this.btn_rechercher = new System.Windows.Forms.Button();
+            this.btn_ajouter = new System.Windows.Forms.Button();
+            this.btn_modifier = new System.Windows.Forms.Button();
+            this.btn_supprimer = new System.Windows.Forms.Button();
+            this.btn_quitter = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.txt_tel);
+            this.groupBox1.Controls.Add(this.txt_ville);
+            this.groupBox1.Controls.Add(this.txt_prenom);
+            this.groupBox1.Controls.Add(this.txt_nom);
+            this.groupBox1.Controls.Add(this.txt_cin);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(30, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(682, 265);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CIN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "NOM";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "PRENOM";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "VILLE";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 225);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "TEL";
+            // 
+            // txt_cin
+            // 
+            this.txt_cin.Location = new System.Drawing.Point(188, 38);
+            this.txt_cin.Name = "txt_cin";
+            this.txt_cin.Size = new System.Drawing.Size(141, 24);
+            this.txt_cin.TabIndex = 0;
+            // 
+            // txt_nom
+            // 
+            this.txt_nom.Location = new System.Drawing.Point(188, 83);
+            this.txt_nom.Name = "txt_nom";
+            this.txt_nom.Size = new System.Drawing.Size(141, 24);
+            this.txt_nom.TabIndex = 1;
+            // 
+            // txt_prenom
+            // 
+            this.txt_prenom.Location = new System.Drawing.Point(188, 126);
+            this.txt_prenom.Name = "txt_prenom";
+            this.txt_prenom.Size = new System.Drawing.Size(141, 24);
+            this.txt_prenom.TabIndex = 2;
+            // 
+            // txt_ville
+            // 
+            this.txt_ville.Location = new System.Drawing.Point(188, 168);
+            this.txt_ville.Name = "txt_ville";
+            this.txt_ville.Size = new System.Drawing.Size(141, 24);
+            this.txt_ville.TabIndex = 3;
+            // 
+            // txt_tel
+            // 
+            this.txt_tel.Location = new System.Drawing.Point(188, 218);
+            this.txt_tel.Name = "txt_tel";
+            this.txt_tel.Size = new System.Drawing.Size(141, 24);
+            this.txt_tel.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_quitter);
+            this.groupBox2.Controls.Add(this.btn_supprimer);
+            this.groupBox2.Controls.Add(this.btn_modifier);
+            this.groupBox2.Controls.Add(this.btn_ajouter);
+            this.groupBox2.Controls.Add(this.btn_rechercher);
+            this.groupBox2.Controls.Add(this.btn_nouveau);
+            this.groupBox2.Location = new System.Drawing.Point(474, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(155, 236);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // btn_nouveau
+            // 
+            this.btn_nouveau.Location = new System.Drawing.Point(26, 19);
+            this.btn_nouveau.Name = "btn_nouveau";
+            this.btn_nouveau.Size = new System.Drawing.Size(110, 23);
+            this.btn_nouveau.TabIndex = 0;
+            this.btn_nouveau.Text = "Nouveau";
+            this.btn_nouveau.UseVisualStyleBackColor = true;
+            this.btn_nouveau.Click += new System.EventHandler(this.btn_nouveau_Click);
+            // 
+            // btn_rechercher
+            // 
+            this.btn_rechercher.Location = new System.Drawing.Point(26, 56);
+            this.btn_rechercher.Name = "btn_rechercher";
+            this.btn_rechercher.Size = new System.Drawing.Size(110, 23);
+            this.btn_rechercher.TabIndex = 0;
+            this.btn_rechercher.Text = "Rechercher";
+            this.btn_rechercher.UseVisualStyleBackColor = true;
+            this.btn_rechercher.Click += new System.EventHandler(this.btn_rechercher_Click);
+            // 
+            // btn_ajouter
+            // 
+            this.btn_ajouter.Location = new System.Drawing.Point(26, 93);
+            this.btn_ajouter.Name = "btn_ajouter";
+            this.btn_ajouter.Size = new System.Drawing.Size(110, 23);
+            this.btn_ajouter.TabIndex = 0;
+            this.btn_ajouter.Text = "Ajouter";
+            this.btn_ajouter.UseVisualStyleBackColor = true;
+            this.btn_ajouter.Click += new System.EventHandler(this.btn_ajouter_Click);
+            // 
+            // btn_modifier
+            // 
+            this.btn_modifier.Location = new System.Drawing.Point(26, 130);
+            this.btn_modifier.Name = "btn_modifier";
+            this.btn_modifier.Size = new System.Drawing.Size(110, 23);
+            this.btn_modifier.TabIndex = 0;
+            this.btn_modifier.Text = "Modifier";
+            this.btn_modifier.UseVisualStyleBackColor = true;
+            this.btn_modifier.Click += new System.EventHandler(this.btn_modifier_Click);
+            // 
+            // btn_supprimer
+            // 
+            this.btn_supprimer.Location = new System.Drawing.Point(26, 167);
+            this.btn_supprimer.Name = "btn_supprimer";
+            this.btn_supprimer.Size = new System.Drawing.Size(110, 23);
+            this.btn_supprimer.TabIndex = 0;
+            this.btn_supprimer.Text = "Supprimer";
+            this.btn_supprimer.UseVisualStyleBackColor = true;
+            this.btn_supprimer.Click += new System.EventHandler(this.btn_supprimer_Click);
+            // 
+            // btn_quitter
+            // 
+            this.btn_quitter.Location = new System.Drawing.Point(26, 204);
+            this.btn_quitter.Name = "btn_quitter";
+            this.btn_quitter.Size = new System.Drawing.Size(110, 23);
+            this.btn_quitter.TabIndex = 1;
+            this.btn_quitter.Text = "Quitter";
+            this.btn_quitter.UseVisualStyleBackColor = true;
+            this.btn_quitter.Click += new System.EventHandler(this.btn_quitter_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 306);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(682, 162);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 480);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Mise à jour des clients";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_quitter;
+        private System.Windows.Forms.Button btn_supprimer;
+        private System.Windows.Forms.Button btn_modifier;
+        private System.Windows.Forms.Button btn_ajouter;
+        private System.Windows.Forms.Button btn_rechercher;
+        private System.Windows.Forms.Button btn_nouveau;
+        private System.Windows.Forms.TextBox txt_tel;
+        private System.Windows.Forms.TextBox txt_ville;
+        private System.Windows.Forms.TextBox txt_prenom;
+        private System.Windows.Forms.TextBox txt_nom;
+        private System.Windows.Forms.TextBox txt_cin;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
